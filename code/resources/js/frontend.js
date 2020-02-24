f2d131510b2ca95e5ed9aa1ea63d5b6d9b79be1c
Peter DuCharme <peter@area17.com>
2020-02-24 14:29:11 -0500

    ignore

---
+++ code/resources/js/frontend.js
@@ -0,0 +1,12 @@
+require('./bootstrap');
+import Vue from "vue";
+import App from "./App.vue";
+
+require('./image_html');
+
+//Vue.config.productionTip = false;
+//
+//new Vue({
+//    render: h => h(App)
+//}).$mount("#app");
+//
